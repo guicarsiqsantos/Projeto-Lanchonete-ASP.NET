c54ede1830a17fe845ad39f81f348283810cf939
Guilherme <guicarsiqsantos@gmail.com>
2023-05-10 10:03:23 -0300

    Subindo projeto

---
+++ AppLanchonete/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace appLanchonete.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AppLanchonete/Views/Shared/Carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace appLanchonete.Views.Shared
+{
+    public class CarouselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
